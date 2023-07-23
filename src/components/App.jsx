22f82b8132be6ccf7c5459b1cb84b3d6165e5834
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-07-23 23:59:34 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,88 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template goit-react-hw-02-phonebook 
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactList from '../components/ContactList/ContactList';
+import ContactForm from '../components/ContactForm/ContactForm';
+import Filter from '../components/Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, number, contacts } = this.state;
+
+    if (contacts.find((contact) => contact.name.toLowerCase() === name.toLowerCase())) {
+      alert(`Contact with name ${name} already exists!`);
+      return;
+    }
+
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
+
+    const contact = {
+      id: this.generateUniqueId(),
+      name,
+      number,
+    };
+
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, contact],
+      name: '',
+      number: '',
+    }));
+  };
+
+  handleDeleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
+
+  handleFilterChange = (e) => {
+    this.setState({ filter: e.target.value });
+  };
+
+  generateUniqueId = () => {
+    const nanoid = require('nanoid');
+    return nanoid.nanoid();
+  };
+
+  render() {
+    const { contacts, name, number, filter } = this.state;
+
+    const filteredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <ContactForm
+          name={name}
+          number={number}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.handleFilterChange} />
+        <ContactList contacts={filteredContacts} onDeleteContact={this.handleDeleteContact} />
+      </div>
+    );
+  }
+}
+
+export default App;
