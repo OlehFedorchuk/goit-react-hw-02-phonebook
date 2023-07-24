91835f531fe1fc60f03e066bfbdabcaf60d15df7
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-07-24 09:30:06 +0100

    update

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 const Filter = ({ value, onChange }) => {
   return (
     <label>
@@ -8,5 +10,8 @@ const Filter = ({ value, onChange }) => {
   );
 };
 
-
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
 export default Filter;
